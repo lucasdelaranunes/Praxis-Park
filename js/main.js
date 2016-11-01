3053a963e2105c913173d61125f7b6de0cbb5b62
Eric Ribeiro <ericribeiro@outlook.com.br>
2016-11-01 06:00:50 -0200

    fixed some little things

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 function loadXMLDoc() {
-	var xmlhttp = new XMLHttpRequest();
-	var cod = document.getElementById("cod").value;
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	var email = document.getElementById("email").value;
-	var celnumber = document.getElementById("celnumber").value;
-	
-    xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/prp_cliente", true);
-	xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+			var xmlhttp = new XMLHttpRequest();
+			var cod = document.getElementById("cod").value;
+			var username = document.getElementById("username").value;
+			var password = document.getElementById("password").value;
+			var email = document.getElementById("email").value;
+			var celnumber = document.getElementById("celnumber").value;
+			
+            xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/prp_cliente", true);
+			xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-	var dataToSend = '_data={"cdcliente": "' + cod + '", "nmcliente":  "' + username + '", "senhacliente":  "' + password + '", "emailcliente": "' + email + '", "celularcliente": "' + celnumber + '"}';
+			var dataToSend = '_data={"cdcliente": "' + cod + '", "nmcliente":  "' + username + '", "senhacliente":  "' + password + '", "emailcliente": "' + email + '", "celularcliente": "' + celnumber + '"}';
 
-	xmlhttp.send(dataToSend);
-}
+			xmlhttp.send(dataToSend);
+		}
