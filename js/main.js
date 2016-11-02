9c79c8ba89c12abdc6344ddd38149b815afa1113
Eric Ribeiro <ericribeiro@outlook.com.br>
2016-11-02 13:16:24 -0200

    main.js was updated according to the changes made in the DB

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function loadXMLDoc() {
 	var xmlhttp = new XMLHttpRequest();
-	var cod = document.getElementById("cod").value;
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	var email = document.getElementById("email").value;
@@ -9,7 +8,6 @@ function loadXMLDoc() {
     xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/prp_cliente", true);
 	xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-	var dataToSend = '_data={"cdcliente": "' + cod + '", "nmcliente":  "' + username + '", "senhacliente":  "' + password + '", "emailcliente": "' + email + '", "celularcliente": "' + celnumber + '"}';
-
+	var dataToSend = '_data={"nmcliente":  "' + username + '", "senhacliente":  "' + password + '", "emailcliente": "' + email + '", "celularcliente": "' + celnumber + '"}';
 	xmlhttp.send(dataToSend);
 }
