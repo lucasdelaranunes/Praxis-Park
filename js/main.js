27ade7c551189941f5a5920584ed7087dd95538a
Eric Ribeiro <ericribeiro@outlook.com.br>
2016-11-02 13:25:40 -0200

    changed the dataToSend parameters, maintainability is improved

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ function loadXMLDoc() {
     xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/prp_cliente", true);
 	xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-	var dataToSend = '_data={"nmcliente":  "' + username + '", "senhacliente":  "' + password + '", "emailcliente": "' + email + '", "celularcliente": "' + celnumber + '"}';
+	var data = {
+		"nmcliente": username,
+		"senhacliente": password,
+		"emailcliente": email,
+		"celularcliente": celnumber
+	}
+
+	var dataToSend = '_data=' + JSON.stringify(data);
+	
 	xmlhttp.send(dataToSend);
 }
